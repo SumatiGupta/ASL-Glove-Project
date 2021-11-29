9852b2ad179d9dfb5cb559b9f22a85f9e6a115dc
nashsth <37915975+nashsth@users.noreply.github.com>
2021-11-28 23:49:20 -0500

    Add files via upload
    This file is the workaround to a certain problem we were having with the UART0 receiver module (more details further down). This file works with the Integrated Glove Final file. 
    
    The problem with the UART0 receiver module that we were trying to solve with this workaround is the following:
    
    The receiver array does not synchronize with the transmitter array. That is, the transmitter MCU sends information regarding the flex sensors to the receiver MCU in the form of an array. The transmitter MCU has a transmit array, and the receiver MCU has a receive array. Logically, the two arrays should be identical. However, it was observed that the data in the receive array are not perfectly aligned with that in the transmit array. 
    
    For instance, suppose that in the transmit array, we have the values 0x1F, 0x2E, 0x3D, and 0x4C in indices 1,2,3 and 4. However, in the receive array, these values appear in indices, say, 3,4,5 and 6. Furthermore, these values then change their position in the receive array --  they shift to, say, 2,3,4, and 5. Then they shift to 4,5,6, and 7, and so on. The receive array values are not in sync with the transmit array values.
    
    The issue was that the receiver MCU needed to poll the UART0 rx pin perfectly in sync with the transmitter MCU UART0 tx pin. If the rx pin was not polled at the correct time, the receiver would lose data (or the data would arrive late in the next iteration) and hence the wrong data would fill the wrong index. The receiver MCU needed to know exactly when to poll so that the correct data will be received. 
    
    This file attempts to fix this issue by using UART1 as the module that synchronizes the polling done by UART0. Whenever UART1 receives any character, an interrupt is generated and this signals UART0 to start polling.

---
+++ Latest_RX_With_UART1_Interrupt/UART.c
@@ -0,0 +1,163 @@
+#include "UART.h"
+
+//UART RX
+uint8_t receivedbuffer[11];
+uint16_t receivedADCread[5]; 
+uint8_t receivedcal_val;
+volatile int received_go_signal = 0; 
+volatile int raise_this_flag = 0;
+
+
+
+
+void Init_UART(void)
+{
+	// Let's use UART0 and PTA
+	// Enable clock gating for UART0 and Port A
+	
+	uint16_t sbr; 
+	
+	//We use Receive data from glove and maybe eventually Transmit (for error check) 
+	//Since the UART can only take 8 bits, and we have 5 important 16 bit values that 
+	//need to be sent, we would have to fill D reg with each half of an array element
+	//making it a total of 5*2 times D needs to be updated. 
+	SIM->SCGC4 |= SIM_SCGC4_UART0_MASK; 										
+	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;											
+	
+	// Make sure transmitter and receiver are disabled before configuring
+	UART0->C2 &= ~UART_C2_TE_MASK & ~UART_C2_RE_MASK; 		
+	
+	// UART 1 has to use Bus Clock, clock can't be configured
+	// according to table 5-2 (page 122) of reference manual 
+	// and absence of macors
+  // but UART0 clock needs to be set - to 48 MHz clock in this case
+	SIM->SOPT2 |= SIM_SOPT2_UART0SRC(1);
+	//SIM->SOPT2 |= SIM_SOPT2_PLLFLLSEL_MASK;
+	
+	// Set pins to UART1 Rx and Tx
+	PORTE->PCR[RX_PIN] |= PORT_PCR_MUX(4); // Rx
+	
+	// Set baud rate and oversampling ratio
+	sbr = (uint16_t)((CLOCK)/(BAUD_RATE * OVERSAMPLE_RATE)) - 32; 			
+	UART0->BDH &= ~UART0_BDH_SBR_MASK;
+	UART0->BDH |= UART0_BDH_SBR(0x1F & (sbr>>8));
+	UART0->BDL = UART0_BDL_SBR(sbr);
+	UART0->C4 |= UART0_C4_OSR(OVERSAMPLE_RATE-1); //UARTLP_C4_OSR(x) is same as UART1_C4_OSR(x) and since C4 reg all UART is same, it should work 	
+	
+	//LIN break is high whenever a break is detected. 
+	//I believe this occurs when there is empty space in a transmission or recieve line 
+	//this will set LBKDIF bit in UARTx_S2 as a high. 
+	//setting LBKDIE bit in BDH reg will cause an interrupt if LBKDIF is high
+	UART0->BDH |= UART0_BDH_LBKDIE(0);  // for now let's disable it 
+	UART0->BDH |= UART0_BDH_RXEDGIE(0); // Disable interrupts for RX active edge
+	UART0->BDH |= UART0_BDH_SBNS(0);   // one stop-bit - only two options 1 or 2
+																	   // Its logic level is the same as the signal's idle state, i.e., logic high
+	UART0->C1 |= UART0_C1_M(0); //Data bit mode - 8 bits - In 8-bit data mode, the shift register holds a
+														  //start bit, eight data bits, and a stop bit.
+  UART0->C1 |= UART0_C1_LOOPS(0); //0: Normal operation - RxD and TxD use separate pins.
+	UART0->C1	|= UART0_C1_PE(0);	//Don't use parity bit
+																//We can use parity bit but will need to change bit mode to 9 - The 9-bit data mode is typically used with parity to allow eight bits of data plus the parity in the ninth bit
+																//When parity is enabled, the bit immediately before the stop bit is treated as the parity bit
+																// a little confused about D being only 8 bits long
+																// will also need to set PT bit in C1 to parity odd or even and enable interrupt for parity (PEIE) in C3 
+																
+	// Don't enable interrupts for errors															
+	UART0->C3 = UART0_C3_ORIE(0)| UART0_C3_NEIE(0) | UART0_C3_FEIE(0) | UART0_C3_PEIE(0);															
+
+	// Clear error flags
+	UART0->S1 |= UART0_S1_OR(1) | UART0_S1_NF(1) | UART0_S1_FE(1) | UART0_S1_PF(1); //To clear these flags write logic one to them 
+
+  UART0->S2 = UART0_S2_MSBF(0) | UART0_S2_RXINV(0); //Send LSB first, do not invert received data
+	
+	UART0->C2 |= UART_C2_RE(1); // Enable UART receiver
+	
+	
+}
+
+
+void Init_UART1(void)
+{
+	// rx pin (PTC3)
+	
+	uint16_t sbr; 
+	
+	SIM->SCGC4 |= SIM_SCGC4_UART1_MASK;  										
+	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;	
+	
+	UART1->C2 &= ~UART_C2_TE_MASK & ~UART_C2_RE_MASK;
+	
+	PORTC->PCR[3] |= PORT_PCR_MUX(3); //3 is the RX pin for UART1; alternative 3 is the selection option for UART1 rx
+	
+	sbr = (uint16_t)((CLOCK)/(BAUD_RATE * 16)) - 32; 			
+	UART1->BDH &= ~UART_BDH_SBR_MASK;
+	UART1->BDH |= UART_BDH_SBR(0x1F & (sbr>>8));
+	UART1->BDL = UART_BDL_SBR(sbr);
+	//UART1->C4 |= UARTLP_C4_OSR(OVERSAMPLE_RATE-1);
+	
+	UART1->BDH |= UART_BDH_LBKDIE(0);  
+	UART1->BDH |= UART_BDH_RXEDGIE(0); 
+	UART1->BDH |= UART_BDH_SBNS(0);
+	
+	
+	UART1->C1 |= UART_C1_M(0); 												  
+  UART1->C1 |= UART_C1_LOOPS(0); 
+	UART1->C1	|= UART_C1_PE(0);
+	
+	UART1->C3 = UART_C3_ORIE(0)| UART_C3_NEIE(0) | UART_C3_FEIE(0) | UART_C3_PEIE(0);	
+	
+	UART1->S2 &= ~UARTLP_S2_MSBF_MASK; 
+	UART1 -> S2 = UART_S2_RXINV(0); 
+	UART1->C2 |= UART_C2_RE(1) | UART_C2_RIE(1);
+	 
+	NVIC_SetPriority(UART1_IRQn, 0);
+	NVIC_ClearPendingIRQ(UART1_IRQn);
+	NVIC_EnableIRQ(UART1_IRQn);
+	__enable_irq(); 
+}
+
+
+
+void UART1_IRQHandler(void)
+{
+	
+	if((UART1 -> S1) & (UART_S1_RDRF_MASK))
+	{
+		received_go_signal = UART1 -> D; 
+		if(received_go_signal)
+			raise_this_flag = 1; 
+		else
+			raise_this_flag = 0; 
+	}
+
+}
+
+
+
+
+
+
+void UART0_Receive_DMA(int index) {
+		
+		
+		while (!(UART0->S1 & UART0_S1_RDRF_MASK));
+	
+		receivedbuffer[index] = UART0->D;
+ 
+}
+
+void CombineReceiveBuffer(){
+	
+	receivedADCread[pinky_index] =  COMBINE(receivedbuffer[1], receivedbuffer[0]);
+	receivedADCread[ring_index] =   COMBINE(receivedbuffer[3], receivedbuffer[2]);
+	receivedADCread[middle_index] = COMBINE(receivedbuffer[5], receivedbuffer[4]);
+	receivedADCread[index_index] =  COMBINE(receivedbuffer[7], receivedbuffer[6]);
+	receivedADCread[thumb_index] =  COMBINE(receivedbuffer[9], receivedbuffer[8]);
+	receivedcal_val = receivedbuffer[10]; 
+}
+
+
+void UART0_Receive_char (void)
+{
+	while (!(UART0->S1 & UART0_S1_RDRF_MASK));
+		//Receive_Done = UART0->D;
+}
---
+++ Latest_RX_With_UART1_Interrupt/main.c
@@ -0,0 +1,54 @@
+#include "main.h" 
+
+#define LED_RED (1) //on port A
+#define LED_YELLOW (2) //on port A
+#define	LED_GREEN	(12) //on port A
+#define	LED_GREEN2 (4) //on port A
+#define LED_BLUE (4) // on port D 
+#define MASK(x) (1UL << (x))
+
+int index; 
+
+int main (void)
+{
+	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTD_MASK; 
+	
+	//Select GPIO pin for 6 LED outputs
+	PORTA->PCR[LED_RED] &= ~PORT_PCR_MUX_MASK; 
+	PORTA->PCR[LED_RED] |= PORT_PCR_MUX(1);
+	PORTA->PCR[LED_YELLOW] &= ~PORT_PCR_MUX_MASK; 
+	PORTA->PCR[LED_YELLOW] |= PORT_PCR_MUX(1);
+	PORTA->PCR[LED_GREEN] &= ~PORT_PCR_MUX_MASK; 
+	PORTA->PCR[LED_GREEN] |= PORT_PCR_MUX(1);
+	PORTA->PCR[LED_GREEN2] &= ~PORT_PCR_MUX_MASK; 
+	PORTA->PCR[LED_GREEN2] |= PORT_PCR_MUX(1);
+	PORTD->PCR[LED_BLUE] &= ~PORT_PCR_MUX_MASK; 
+	PORTD->PCR[LED_BLUE] |= PORT_PCR_MUX(1);
+	
+	//set LED bits as outputs 
+	PTA->PDDR |= MASK(LED_RED) | MASK(LED_YELLOW) | MASK(LED_GREEN) | MASK(LED_GREEN2);
+	PTD->PDDR |= MASK(LED_BLUE);
+	
+	//Switch off all LEDS intially 
+	PTA->PCOR = MASK(LED_RED) | MASK(LED_YELLOW)| MASK(LED_GREEN) | MASK(LED_GREEN2);
+	PTD->PCOR |= MASK(LED_BLUE); 
+	
+	Init_UART();
+	Init_UART1();
+
+	while(1)
+	{
+		if(received_go_signal)
+		{
+			UART0_Receive_DMA(index);
+			index++;
+			
+			if(index > 10)
+			{
+				index = 0; 
+				CombineReceiveBuffer();
+			}
+		}	
+		
+	}
+}
