5c1a9e25adecd9c74ff1e0739cb68146c6e615c5
SumatiGupta <42660697+SumatiGupta@users.noreply.github.com>
2022-02-21 16:09:58 -0500

    Delete FinalProjectCode directory

--- FinalProjectCode/Glove complete'/Objects/pit.d
+++
@@ -1,8 +0,0 @@
-./objects/pit.o: pit.c pit.h \
-  C:\Users\sumat\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Users\sumat\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0plus.h \
-  C:\Users\sumat\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h \
-  C:\Users\sumat\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\Users\sumat\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armclang.h \
-  RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h adc.h read.h
--- FinalProjectCode/LED screen complete/main.h
+++
@@ -1,8 +0,0 @@
-#include <MKL25Z4.h>
-#include "dma.h"
-#include "cal.h"
-#include "UART.h"
-#include "adc.h"
-#include "16Segment.h"
-
-extern int screen_input;
