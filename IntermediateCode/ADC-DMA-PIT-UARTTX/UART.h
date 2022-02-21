ad47a800ee4e5fb9d9befa931ff2af157d35b25a
SumatiGupta <42660697+SumatiGupta@users.noreply.github.com>
2022-02-21 15:51:30 -0500

    Add files via upload

---
+++ IntermediateCode/ADC-DMA-PIT-UARTTX/UART.h
@@ -0,0 +1,36 @@
+#ifndef UART_H
+#define UART_H
+
+#include <MKL25Z4.h>
+#include "dma.h"
+
+//declarations in UART RX
+
+#define BAUD_RATE     	(9600)
+#define OVERSAMPLE_RATE (25)
+#define CLOCK     			(24000000)
+
+#define TX_PIN          (2) //on port A - ALT 2 is UART0_Tx
+
+#define LSB(x)            (uint8_t)(x & 0x00FF)
+#define MSB(x)            (uint8_t)((x & 0xFF00) >> 8)
+#define GLOVEMASK(x)      ((x) >> 16)
+#define COMBINE(MSB, LSB) (uint16_t)(((uint16_t)MSB << 8) | LSB)
+
+void Init_UART(void); 
+void UART0_Transmit_DMA(int);
+void TransmitBufferCreate(void);
+void UART0_Transmit_OneByte(void);
+
+extern uint8_t transmitbuffer[11];
+extern int shiftedpinky; 
+extern int shiftedring; 
+extern int shiftedmiddle; 
+extern int shiftedindex; 
+extern int shiftedthumb; 
+extern uint8_t cal_val; 
+
+#endif 
+
+//Note: added ifndef and endif to get rid of "include nested too deeply"
+//error
